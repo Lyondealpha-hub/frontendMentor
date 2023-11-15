d2bb2dc5105dd0d1950134f423676d55566f04ce
PERCY KONOTEY DJIWORNU <percy.konotey@innorik.com>
2023-11-15 12:10:15 +0000

    update

--- teams/src/App.tsx
+++ teams/src/App.tsx
@@ -1,36 +1,12 @@
-<<<<<<< HEAD
-// import 'devextreme/dist/css/dx.light.css';
-import MyRoutes from "./routers";
-// import { CardS } from "./templates/Card";
-// import { CardTemplates } from "./templates/cardTemplates";
-// import MyRoutes from './routers';
-=======
->>>>>>> 306b245f579ec03f97da09fc5fac467c21533501
 
 import "devextreme/dist/css/dx.light.css";
 import MyRoutes from './routers';
 const App = () => {
   return (
-<<<<<<< HEAD
-     <div>
-      <MyRoutes />
-     </div>
-    // <div className="w-full flex justify-between ">
-    //   {/* left container */}
-    //   <div className="w-[100%] px-10 py-10">
-    //     <CardTemplates title={"frontend dev"} content={"hiiii"} />
-    //   </div>
-    //   {/* right container  */}
-    //   <div className="w-[30%] px-10 py-5  ">
-    //     <CardS/>
-    //   </div>
-    // </div>
-=======
     <div className="w-full h-full bg-gray-300 ">
       <MyRoutes />
     </div>
    
->>>>>>> 306b245f579ec03f97da09fc5fac467c21533501
   );
 };
 
---
+++ teams/src/templates/Timer.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const Timer = () => {
+    const [starttime , setStarttime] = useState<string>('Display Time');
+    const [endtime , setEndtime] = useState<string>('Display Time');
+    const [timeSpent , setTimespent] = useState<string>('Display ')
+  return (
+    <div className="shadow-lg border p-8 px-10 rounded-lg w-[100%] h-[90%]">
+      {/* <label className=" text-lg ">{dayOfWeek}</label>
+      <input className="font-bold text-xl " type="date"></input> */}
+      <div className="flex gap-40 justify-center ">
+        <div>
+          <label className="font-bold text-lg">Start Time</label>
+          <div className="border rounded-lg shadow-lg text-center p-2 py-2 ">{starttime}</div>
+        </div>
+        <div>
+          <label className="font-bold text-lg">End Time</label>
+          <div className="border rounded-lg shadow-lg text-center p-2 py-2">{endtime}</div>
+        </div>
+        <div>
+          <label className="font-bold text-lg">Time Spent</label>
+          <div className="border rounded-lg shadow-lg text-center p-2 py-2">{timeSpent}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
