21f81e112d7a57d67047856e8d16cdb2b32f3620
NanaAdjoa-hub <152899404+NanaAdjoa-hub@users.noreply.github.com>
2023-12-30 22:48:40 +0000

    fix error

--- teams/src/components/Help.tsx
+++ teams/src/components/Help.tsx
@@ -30,9 +30,21 @@ export const Help = () => (
                     <>
                       <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
                         <span>What is your refund policy?</span>
+
+                          {
+                            open ? 
+
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
-                        </svg>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+                          </svg>
+                            :
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
+
+                          }
+
+
 
                       </Disclosure.Button>
                       <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
